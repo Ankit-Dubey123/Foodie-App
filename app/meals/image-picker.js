3f05a4ff81d5506c54c7090e3ddd0ad93c8f766d
Ankit Dubey <ankit.dubey912518@gmail.com>
2025-08-15 19:13:03 +0530

    initial commit

---
+++ app/meals/image-picker.js
@@ -0,0 +1,24 @@
+import classes from "./image-picker.module.css";
+
+export default function ImagePicker({ label, name }) {
+  function handlePickClick () {
+    
+  }
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+        />
+        <button className={classes.button} type="submit" onClick={handlePickClick}>
+          Pick an image
+        </button>
+      </div>
+    </div>
+  );
+}
